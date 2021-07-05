8a5061f13528325ef86dbc12f8702e29dbfee994
Utkarsh Kumar <utkarsh18e@iitg.ac.in>
2021-07-06 01:33:08 +0530

    logical error resolved

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const homeController=require('../controllers/home_controller');
 
 router.use('/user',require('./user'));
-router.get('/profile',homeController.profile);
+// router.get('/profile',homeController.profile);
 router.get('/home',homeController.home);
 
 module.exports = router;
